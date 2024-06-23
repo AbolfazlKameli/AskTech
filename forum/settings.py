800302ac0c931142867380f9f0984294862bac5e
abolfazl <abolfazlkameli0@gmail.com>
2024-06-24 01:31:44 +0330

    project: add SMTP configs.

--- forum/settings.py
+++ forum/settings.py
@@ -147,3 +147,12 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'a forum about Technology issues',
     'VERSION': '0.0.1'
 }
+
+# SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bozorgmehrkameli@gmail.com'
+EMAIL_HOST_PASSWORD = 'tiguebokfverofdu'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'AskTech'
