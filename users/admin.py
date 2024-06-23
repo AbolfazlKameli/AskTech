6eb4db67a468d71a51459903ff3503be5d5878f6
abolfazl <abolfazlkameli0@gmail.com>
2024-06-24 01:31:17 +0330

    project: add otpcode model.

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .forms import UserChangeForm, UserCreationForm
-from .models import User
+from .models import User, OTPCode
 
 
 class UserAdmin(BaseUserAdmin):
@@ -34,3 +34,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(OTPCode)
+
--- users/models.py
+++ users/models.py
@@ -21,3 +21,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def is_staff(self):
         return self.is_admin
+
+
+class OTPCode(models.Model):
+    email = models.EmailField()
+    otp = models.CharField(max_length=6)
+    expire = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.email}-{self.otp}'
