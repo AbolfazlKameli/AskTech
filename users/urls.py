c4ba40f93cda776003b3087b878c5a3d062fdeb2
abolfazl <abolfazlkameli0@gmail.com>
2024-06-26 06:26:28 +0330

    refacotr: refacotr token urls.

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
 app_name = 'users'
 
+token = [
+    path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('block_token/', views.BlockTokenAPI.as_view(), name='token_block'),
+]
+
 urlpatterns = [
     path('', views.UsersListAPI.as_view(), name='users_list'),
+    path('token/', include(token)),
     path('register/', views.UserRegisterAPI.as_view(), name='user_register'),
     path('register/verify/<str:token>/', views.UserRegisterVerifyAPI.as_view(), name='user_register_verify'),
     path('register/resend_email/', views.ResendVerificationEmailAPI.as_view(), name='user_register_resend_email'),
     path('change_password/', views.ChangePasswordAPI.as_view(), name='change_password'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
