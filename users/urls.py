5cccd04635924a8eb64a8f0e4ae900e4d4945c8b
abolfazl <abolfazlkameli0@gmail.com>
2024-06-26 05:39:20 +0330

    fix: move token urls to users app.

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path('register/verify/<str:token>/', views.UserRegisterVerifyAPI.as_view(), name='user_register_verify'),
     path('register/resend_email/', views.ResendVerificationEmailAPI.as_view(), name='user_register_resend_email'),
     path('change_password/', views.ChangePasswordAPI.as_view(), name='change_password'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
