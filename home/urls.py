137d9d11b3f2d1291923461a9cc4a7dace1cb39a
abolfazl <abolfazlkameli0@gmail.com>
2024-07-08 15:37:28 +0330

    refactor: refactor imports.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.HomeAPI.as_view(), name='home'),
+    path('', views.QuestionListAPI.as_view(), name='home'),
 ]
