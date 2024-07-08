11ec6fe87c23fba982c42a773b960b312d841f28
abolfazl <abolfazlkameli0@gmail.com>
2024-07-08 16:34:00 +0330

    fix: change user field to owner.

--- home/admin.py
+++ home/admin.py
@@ -5,20 +5,20 @@ from .models import Question, Answer
 
 class AnswerInline(admin.StackedInline):
     model = Answer
-    raw_id_fields = ('user', 'question')
+    raw_id_fields = ('owner', 'question')
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'body', 'user', 'id', 'created')
-    search_fields = ('user__username', 'user__email', 'title')
-    raw_id_fields = ('user',)
+    list_display = ('title', 'body', 'owner', 'id', 'created')
+    search_fields = ('owner__username', 'owner__email', 'title')
+    raw_id_fields = ('owner',)
     prepopulated_fields = {'slug': ('title',)}
     inlines = (AnswerInline,)
 
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'body', 'question')
-    search_fields = ('user__username', 'user__email', 'body')
-    raw_id_fields = ('question', 'user')
+    list_display = ('owner', 'body', 'question')
+    search_fields = ('owner__username', 'owner__email', 'body')
+    raw_id_fields = ('question', 'owner')
--- home/models.py
+++ home/models.py
@@ -4,25 +4,25 @@ from users.models import User
 
 
 class Question(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
     title = models.CharField(max_length=500)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(max_length=30, blank=True, null=True)
+    slug = models.SlugField(max_length=30, blank=True, null=True, unique=True)
 
     # TODO: add and tags or categories.
 
     def __str__(self):
-        return f'{self.user} - {self.title[:30]}...'
+        return f'{self.owner} - {self.title[:30]}...'
 
 
 class Answer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question')
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user} - {self.body[:20]}... - {self.question}'
+        return f'{self.owner} - {self.body[:20]}... - {self.question}'
