1eb59681cc09738fe3722085d905e390d7a215e3
abolfazl <abolfazlkameli0@gmail.com>
2024-07-08 12:44:25 +0330

    project: add answer inline and answer admin page.

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Answer
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    raw_id_fields = ('user', 'question')
 
 
 @admin.register(Question)
@@ -9,3 +14,11 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'user__email', 'title')
     raw_id_fields = ('user',)
     prepopulated_fields = {'slug': ('title',)}
+    inlines = (AnswerInline,)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'body', 'question')
+    search_fields = ('user__username', 'user__email', 'body')
+    raw_id_fields = ('question', 'user')
