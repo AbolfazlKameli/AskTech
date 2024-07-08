3e05f683b15846519856670c2b08b5c94298851e
abolfazl <abolfazlkameli0@gmail.com>
2024-07-08 17:25:40 +0330

    fix: change related names on Answer model.

--- home/models.py
+++ home/models.py
@@ -18,8 +18,8 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now_add=True)
