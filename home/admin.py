c2860eb873969e8789d7b4d4b03e4e848a34a9df
abolfazl <abolfazlkameli0@gmail.com>
2024-07-10 18:55:12 +0330

    project and refactor: add models and refactor. add AnswerComments model. add short_title and short_body to model question and answer. refactor imports, refactor list_display on QuestionAdmin and AnswerAdmin. add AnswerCommentsInline, add AnswerCommentAdmin.

--- home/admin.py
+++ home/admin.py
@@ -1,24 +1,37 @@
 from django.contrib import admin
 
-from .models import Question, Answer
+from . import models
 
 
 class AnswerInline(admin.StackedInline):
-    model = Answer
+    model = models.Answer
     raw_id_fields = ('owner', 'question')
 
 
-@admin.register(Question)
+class AnswerCommentsInline(admin.StackedInline):
+    model = models.AnswerComment
+    raw_id_fields = ('owner', 'answer', 'reply')
+
+
+@admin.register(models.Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'body', 'owner', 'id', 'created')
+    list_display = ('short_title', 'short_body', 'owner', 'id', 'created')
     search_fields = ('owner__username', 'owner__email', 'title')
     raw_id_fields = ('owner',)
     prepopulated_fields = {'slug': ('title',)}
     inlines = (AnswerInline,)
 
 
-@admin.register(Answer)
+@admin.register(models.Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'body', 'question')
+    list_display = ('owner', 'short_body', 'question')
     search_fields = ('owner__username', 'owner__email', 'body')
     raw_id_fields = ('question', 'owner')
+    inlines = (AnswerCommentsInline,)
+
+
+@admin.register(models.AnswerComment)
+class AnswerCommentAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'answer', 'is_reply')
+    raw_id_fields = ('owner', 'answer', 'reply')
+    search_fields = ('owner__username', 'owner__email', 'body', 'reply__body')
--- home/models.py
+++ home/models.py
@@ -19,6 +19,14 @@ class Question(models.Model):
     def __str__(self):
         return f'{self.owner} - {self.title[:30]}...'
 
+    @property
+    def short_title(self):
+        return f'{self.title[:30]}...'
+
+    @property
+    def short_body(self):
+        return f'{self.body[:30]}...'
+
 
 class Answer(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
@@ -31,4 +39,21 @@ class Answer(models.Model):
         ordering = ('-modified', '-created')
 
     def __str__(self):
-        return f'{self.owner} - {self.body[:20]}... - {self.question.title}'
+        return f'{self.owner} - {self.body[:20]}... - {self.question.title[:30]}'
+
+    @property
+    def short_body(self):
+        return f'{self.body[:30]}...'
+
+
+class AnswerComment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answer_comments')
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='replies', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.owner} - {self.answer.body[:20]}...'
