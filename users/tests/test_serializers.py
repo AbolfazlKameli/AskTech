32604ef1d66a003a98a5cb49c2dfd422055c3595
abolfazl <abolfazlkameli0@gmail.com>
2024-07-30 17:08:03 +0330

    test: add UserSerializer test.

--- users/tests/test_serializers.py
+++ users/tests/test_serializers.py
@@ -1,3 +1,36 @@
+from model_bakery import baker
 from rest_framework.test import APITestCase
-from users.serializers import UserSerializer
+
 from users.models import User
+from users.serializers import UserSerializer
+
+
+class TestUserSerializer(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        baker.make(User, username='username', email='email@gmail.com', is_active=True)
+
+    def test_valid_data(self):
+        data = {'username': 'kevin', 'email': 'kevin@gmail.com', 'password': 'password', 'is_active': True}
+        serializer = UserSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['username'], 'kevin')
+
+    def test_empty_fields(self):
+        serializer = UserSerializer(data={})
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 2)
+
+    def test_invalid_username(self):
+        data = {'username': 'username', 'email': 'another_email@gmail.com', 'password': 'password'}
+        serializer = UserSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertEqual(serializer.errors['username'][0], 'user with this username already exists.')
+
+    def test_invalid_email(self):
+        data = {'username': 'another_username', 'email': 'email@gmail.com', 'password': 'password'}
+        serializer = UserSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertEqual(serializer.errors['email'][0], 'user with this email already exists.')
