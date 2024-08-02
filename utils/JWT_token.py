f48855ff910d48acd0a9917aabb74c04b2a1c0d8
abolfazl <abolfazlkameli0@gmail.com>
2024-08-02 15:35:48 +0330

    fix: fix token lifetime system.

--- utils/JWT_token.py
+++ utils/JWT_token.py
@@ -2,24 +2,13 @@ from datetime import datetime
 
 import jwt
 from django.conf import settings
-from pytz import timezone
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.utils import datetime_to_epoch
 
-
-class CustomRefreshToken(RefreshToken):
-    @classmethod
-    def for_user(cls, user, lifetime=None):
-        token = super().for_user(user)
-        token['email'] = user.email
-        if lifetime:
-            token['expire'] = datetime_to_epoch(datetime.now(tz=timezone('Asia/Tehran')) + lifetime)
-        return token
+from users.serializers import MyTokenObtainPairSerializer
 
 
 def generate_token(user, lifetime=None):
-    refresh = CustomRefreshToken.for_user(user, lifetime)
-    return {"refresh": str(refresh), "token": str(refresh.access_token)}
+    token = MyTokenObtainPairSerializer.get_token(user, lifetime)
+    return {"refresh": str(token), "token": str(token.access_token)}
 
 
 def decode_token(token):
