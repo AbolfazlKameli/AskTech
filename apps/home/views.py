1586442c3ba12a0b970895bf3bb422f67cd4b7ba
abolfazl <abolfazlkameli0@gmail.com>
2024-08-27 22:58:15 +0330

    fix/base: use restframework methods.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
