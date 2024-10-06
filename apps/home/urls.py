3559b3a5fe7aa005c43c929b7b94252e1ff01ae5
abolfazl <abolfazlkameli0@gmail.com>
2024-10-06 18:22:44 +0330

    fix: make url names better.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     path('', views.HomeAPI.as_view(), name='home'),
     path('like/<int:answer_id>/', views.LikeAPI.as_view(), name='answer_like'),
     path('dislike/<int:answer_id>/', views.DisLikeAPI.as_view(), name='answer_dislike'),
-    path('accept/<int:answer_id>/', views.AcceptAnswerAPI.as_view(), name='answer_accept'),
-    path('answer/<int:question_id>/', views.CreateAnswerAPI.as_view(), name='answer_create'),
-    path('answer_comment/<int:answer_id>/', views.CreateCommentAPI.as_view(), name='comment_create'),
-    path('reply/<int:comment_id>/', views.CreateReplyAPI.as_view(), name='reply_create'),
-    path('reply/<int:comment_id>/<int:reply_id>/', views.CreateReplyAPI.as_view(), name='reply_create'),
+    path('accept_answer/<int:answer_id>/', views.AcceptAnswerAPI.as_view(), name='answer_accept'),
+    path('create_answer/<int:question_id>/', views.CreateAnswerAPI.as_view(), name='answer_create'),
+    path('create_comment/<int:answer_id>/', views.CreateCommentAPI.as_view(), name='comment_create'),
+    path('create_reply/<int:comment_id>/', views.CreateReplyAPI.as_view(), name='reply_create'),
+    path('create_reply/<int:comment_id>/<int:reply_id>/', views.CreateReplyAPI.as_view(), name='reply_create'),
 ]
 
 router = routers.DefaultRouter()
-router.register('question', views.QuestionViewSet)
+router.register('questions', views.QuestionViewSet)
 router.register('answers', views.AnswerViewSet, basename='answer-viewset')
 router.register('comments', views.CommentViewSet, basename='comments')
 router.register('replies', views.ReplyViewSet, basename='reply')
