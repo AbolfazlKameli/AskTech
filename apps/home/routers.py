a85414240b360773e30d9d431bfe2ca5a57050ea
abolfazl <abolfazlkameli0@gmail.com>
2024-10-12 19:01:01 +0330

    fix: fix URIs design in home app.

--- apps/home/routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-
-class NoListDefaultRouter(DefaultRouter):
-    def get_routes(self, viewset):
-        routes = super().get_routes(viewset)
-        routes = [route for route in routes if route.name != '{basename}-list']
-        return routes
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,23 +1,31 @@
 from django.urls import path
-from .routers import NoListDefaultRouter
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
 app_name = 'home'
 urlpatterns = [
     path('', views.HomeAPI.as_view(), name='home'),
-    path('answer/like/<int:answer_id>/', views.LikeAPI.as_view(), name='answer-like'),
-    path('answer/dislike/<int:answer_id>/', views.DisLikeAPI.as_view(), name='answer-dislike'),
-    path('answer/accept/<int:answer_id>/', views.AcceptAnswerAPI.as_view(), name='answer-accept'),
-    path('answer/create/<int:question_id>/', views.CreateAnswerAPI.as_view(), name='answer-create'),
-    path('comment/create/<int:answer_id>/', views.CreateCommentAPI.as_view(), name='comment-create'),
-    path('reply/create/<int:comment_id>/', views.CreateReplyAPI.as_view(), name='reply-create'),
-    path('reply/create/<int:comment_id>/<int:reply_id>/', views.CreateReplyAPI.as_view(), name='reply-create'),
+
+    # Questions
+    path('questions/<int:question_id>/answers/', views.CreateAnswerAPI.as_view(), name='answer-create'),
+
+    # Answers
+    path('answers/<int:answer_id>/like/', views.LikeAPI.as_view(), name='answer-like'),
+    path('answers/<int:answer_id>/dislike/', views.DisLikeAPI.as_view(), name='answer-dislike'),
+    path('answers/<int:answer_id>/accept/', views.AcceptAnswerAPI.as_view(), name='answer-accept'),
+
+    # comments
+    path('answers/<int:answer_id>/comments/', views.CreateCommentAPI.as_view(), name='comment-create'),
+
+    # Replies
+    path('comments/<int:comment_id>/replies/', views.CreateReplyAPI.as_view(), name='reply-create'),
+    path('comments/<int:comment_id>/replies/<int:reply_id>/', views.CreateReplyAPI.as_view(), name='reply-create'),
 ]
 
-router = NoListDefaultRouter()
-router.register('question', views.QuestionViewSet)
-router.register('answer', views.AnswerViewSet, basename='answer-viewset')
-router.register('comment', views.CommentViewSet, basename='comments')
-router.register('reply', views.ReplyViewSet, basename='reply')
+router = DefaultRouter()
+router.register('questions', views.QuestionViewSet)
+router.register('answers', views.AnswerViewSet, basename='answer-viewset')
+router.register('comments', views.CommentViewSet, basename='comments')
+router.register('replies', views.ReplyViewSet, basename='reply')
 urlpatterns += router.urls
